fc4bf02ee7270d9681e127103b3183262f32932d
Clayton Groeneveld <claytong1214@gmail.com>
2021-05-10 14:46:52 -0500

    Fix formatting not working sometimes

--- advanced-timer.lua
+++ advanced-timer.lua
@@ -46,40 +46,70 @@ end
 function set_time_text(ns, text)
 	local ms = math.floor(ns / 1000000)
 
-	if string.match(text, "%%HH") then
+	if string.match(text, "%%0H") then
 		local hours_infinite = math.floor(ms / 3600000)
 		minutes_infinite = string.format("%02d", hours_infinite)
+		text = string.gsub(text, "%%0H", hours_infinite)
+	end
+
+	if string.match(text, "%%0M") then
+		local minutes_infinite = math.floor(ms / 60000)
+		minutes_infinite = string.format("%02d", minutes_infinite)
+		text = string.gsub(text, "%%0M", minutes_infinite)
+	end
+
+	if string.match(text, "%%0S") then
+		local seconds_infinite = math.floor(ms / 1000)
+		seconds_infinite = string.format("%02d", seconds_infinite)
+		text = string.gsub(text, "%%0S", seconds_infinite)
+	end
+
+	if string.match(text, "%%0h") then
+		local hours = math.floor(ms / 3600000) % 24
+		hours = string.format("%02d", hours)
+		text = string.gsub(text, "%%0h", hours)
+	end
+
+	if string.match(text, "%%0m") then
+		local minutes = math.floor(ms / 60000) % 60
+		minutes = string.format("%02d", minutes)
+		text = string.gsub(text, "%%0m", minutes)
+	end
+
+	if string.match(text, "%%0s") then
+		local seconds = math.floor(ms / 1000) % 60
+		seconds = string.format("%02d", seconds)
+		text = string.gsub(text, "%%0s", seconds)
+	end
+
+	if string.match(text, "%%H") then
+		local hours_infinite = math.floor(ms / 3600000)
 		text = string.gsub(text, "%%HH", hours_infinite)
 	end
 
-	if string.match(text, "%%MM") then
+	if string.match(text, "%%M") then
 		local minutes_infinite = math.floor(ms / 60000)
-		minutes_infinite = string.format("%02d", minutes_infinite)
 		text = string.gsub(text, "%%MM", minutes_infinite)
 	end
 
-	if string.match(text, "%%SS") then
+	if string.match(text, "%%S") then
 		local seconds_infinite = math.floor(ms / 1000)
-		seconds_infinite = string.format("%02d", seconds_infinite)
-		text = string.gsub(text, "%%SS", seconds_infinite)
+		text = string.gsub(text, "%%S", seconds_infinite)
 	end
 
-	if string.match(text, "%%hh") then
+	if string.match(text, "%%h") then
 		local hours = math.floor(ms / 3600000) % 24
-		hours = string.format("%02d", hours)
-		text = string.gsub(text, "%%hh", hours)
+		text = string.gsub(text, "%%h", hours)
 	end
 
-	if string.match(text, "%%mm") then
+	if string.match(text, "%%m") then
 		local minutes = math.floor(ms / 60000) % 60
-		minutes = string.format("%02d", minutes)
-		text = string.gsub(text, "%%mm", minutes)
+		text = string.gsub(text, "%%m", minutes)
 	end
 
-	if string.match(text, "%%ss") then
+	if string.match(text, "%%s") then
 		local seconds = math.floor(ms / 1000) % 60
-		seconds = string.format("%02d", seconds)
-		text = string.gsub(text, "%%ss", seconds)
+		text = string.gsub(text, "%%s", seconds)
 	end
 
 	if string.match(text, "%%d") then
@@ -376,7 +406,7 @@ function script_properties()
 	obs.obs_properties_add_int(props, "minutes", "Minutes (0-59)", 0, 59, 1)
 	obs.obs_properties_add_int(props, "seconds", "Seconds (0-59)", 0, 59, 1)
 	local f_prop = obs.obs_properties_add_text(props, "format", "Format", obs.OBS_TEXT_DEFAULT)
-	obs.obs_property_set_long_description(f_prop, "%d - days\n%hh - hours with leading zero (00..23)\n%h - hours (0..23)\n%HH - hours with leading zero (00..infinity)\n%H - hours (0..infinity)\n%mm - minutes with leading zero (00..59)\n%m - minutes (0..59)\n%MM - minutes with leading zero (00..infinity)\n%M - minutes (0..infinity)\n%ss - seconds with leading zero (00..59)\n%s - seconds (0..59)\n%SS - seconds with leading zero (00..infinity)\n%S - seconds (0..infinity)\n%t - tenths\n%2t - hundredths\n%3t - thousandths")
+	obs.obs_property_set_long_description(f_prop, "%d - days\n%0h - hours with leading zero (00..23)\n%h - hours (0..23)\n%0H - hours with leading zero (00..infinity)\n%H - hours (0..infinity)\n%0m - minutes with leading zero (00..59)\n%m - minutes (0..59)\n%0M - minutes with leading zero (00..infinity)\n%M - minutes (0..infinity)\n%0s - seconds with leading zero (00..59)\n%s - seconds (0..59)\n%0S - seconds with leading zero (00..infinity)\n%S - seconds (0..infinity)\n%t - tenths\n%2t - hundredths\n%3t - thousandths")
 
 	local p = obs.obs_properties_add_list(props, "source", "Text source", obs.OBS_COMBO_TYPE_EDITABLE, obs.OBS_COMBO_FORMAT_STRING)
 	local sources = obs.obs_enum_sources()
@@ -458,7 +488,7 @@ function script_defaults(settings)
 	obs.obs_data_set_default_string(settings, "stop_text", "Starting soon (tm)")
 	obs.obs_data_set_default_string(settings, "mode", "Countdown")
 	obs.obs_data_set_default_string(settings, "a_mode", "Global (timer always active)")
-	obs.obs_data_set_default_string(settings, "format", "%HH:%mm:%ss")
+	obs.obs_data_set_default_string(settings, "format", "%0H:%0m:%0s")
 end
 
 function script_save(settings)
